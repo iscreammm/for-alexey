b6a1ad2d5260224c7e46b64fb939965b76cd156a
Exonor515 <exonor.work@gmail.com>
2023-12-18 15:01:41 +0300

    change main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,17 @@ const printArgs = () => {
 printArgs('Привет', 'мой', 'мир', {lengt: 2}); 
 
 
+function aboba(work) {
+  console.log(1)
+  console.log(2)
+  console.log(3)
+  console.log(4)
+  console.log(5)
+  console.log(6)
+  console.log(7)
+  console.log(8)
+}
+
 /* true + false
 12 / "6"
 "number" + 15 + 3
